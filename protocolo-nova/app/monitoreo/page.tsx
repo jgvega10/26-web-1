d72fe59db7b6ec7780dfd93b9ea5ae9b06e64a69
jgvega10 <jg.vega10@uniandes.edu.co>
2026-02-26 07:39:15 -0500

    archivo .env.local created

--- protocolo-nova/app/monitoreo/page.tsx
+++ protocolo-nova/app/monitoreo/page.tsx
@@ -11,19 +11,6 @@ import { LogRegistro as LogType } from "@/app/types/mission";
 export default function MonitoreoPage() {
   return (
     <div className="p-10">
-<<<<<<< HEAD
-        <div className="font-mono text-emeral-500 text-5xl">Estación Especial Alpha</div>
-        <div className="grid gris-cols-1 h-screen md:grid-cols-2 mt-10">
-            <div>
-                <Formulario/>
-                <LogRegistro logs={[]} />
-            </div>
-
-            <div>
-                <Scanner/>
-            </div>
-        </div>
-=======
       <div className="font-mono text-emerald-500 text-5xl">Estacion especial alpha</div>
       <div className="grid grid-cols-1 h-screen md:grid-cols-2 mt-10">
         <div>
@@ -34,7 +21,6 @@ export default function MonitoreoPage() {
             <Scanner/>
         </div>
       </div>
->>>>>>> 561e369d49def3c5aea40cd54537b966e663e5dc
     </div>
   );
 }
